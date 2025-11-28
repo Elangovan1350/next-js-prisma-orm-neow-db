8afb1f4380c90f4f2d827bff8e0fd505797eb6da
Elangovan1350 <elangovan2019miss@gmail.com>
2025-11-28 20:09:26 +0530

    change4

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -14,17 +14,33 @@ export async function GET() {
 export async function POST(request: Request) {
   const body = await request.json();
   const { email, name } = body;
-  const newUser = await prisma.user.create({
-    data: {
-      email,
-      name,
-    },
-  });
 
-  return new Response(JSON.stringify(newUser), {
-    status: 201,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  try {
+    // creating new users
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+
+    return new Response(JSON.stringify(newUser), {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    // Check if user already exists
+    const exetingUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (exetingUser) {
+      return new Response("User already exists", { status: 409 });
+    }
+    console.error("Error creating user:", error);
+    return new Response("Error creating user", { status: 500 });
+  }
 }
