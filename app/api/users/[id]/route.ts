e12e41bdd0d3447102ec3c07779409736c009c85
Elangovan1350 <elangovan2019miss@gmail.com>
2025-12-08 21:46:02 +0530

    change16

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,9 +1,8 @@
-import { Params } from "next/dist/server/request/params";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: Params }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   return NextResponse.json({ id: id });
